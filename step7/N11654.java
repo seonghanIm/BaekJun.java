2345ff5cc41d92d48be2dcf89b4e21718b6b510a
seonghanIm <80394582+seonghanIm@users.noreply.github.com>
2021-08-31 13:58:08 +0900

    Add files via upload

---
+++ step7/N11654.java
@@ -0,0 +1,18 @@
+package step7;
+
+import java.util.*;
+
+public class N11654 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char s = sc.nextLine().charAt(0);
+		int num = (int)s;
+		System.out.println(num);
+		sc.close();
+
+	}
+
+}
