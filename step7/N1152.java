4b13b807f20f9fd7f9bf1a494fa87fa78beffabb
seonghanIm <80394582+seonghanIm@users.noreply.github.com>
2021-09-01 16:52:27 +0900

    Add files via upload

---
+++ step7/N1152.java
@@ -0,0 +1,21 @@
+package step7;
+
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+
+public class N1152 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num = st.countTokens();
+		System.out.println(num);
+		
+
+	}
+
+}
