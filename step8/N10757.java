130de952221dbd6a7b4be0af551186448f528574
SeongHan Im <80394582+seonghanIm@users.noreply.github.com>
2021-09-21 12:36:15 +0900

    Add files via upload

---
+++ step8/N10757.java
@@ -0,0 +1,25 @@
+package step8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class N10757 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
+		
+		BigInteger result = A.add(B);
+		
+		System.out.println(result);
+
+	}
+
+}
