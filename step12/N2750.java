6142a73165c3f840771833e134f33f53bd2cea98
SeongHan Im <80394582+seonghanIm@users.noreply.github.com>
2021-09-22 11:49:23 +0900

    Add files via upload

---
+++ step12/N2750.java
@@ -0,0 +1,28 @@
+package step12;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N2750 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int []num = new int[N];
+		for(int i=0;i<N;i++)
+		{
+			num[i] = sc.nextInt();
+		}
+		Arrays.sort(num);
+		
+		for(int i=0;i<N;i++)
+		{
+			System.out.println(num[i]);
+		}
+		sc.close();
+
+	}
+
+}
