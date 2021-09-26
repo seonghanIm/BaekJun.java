22c5e5816bf2f69a5d9607449e6ecf0c5c625636
SeongHan Im <80394582+seonghanIm@users.noreply.github.com>
2021-09-27 01:22:13 +0900

    Add files via upload

--- step21/N10816.java
+++ step21/N10816.java
@@ -1,10 +1,10 @@
 package step21;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -14,8 +14,7 @@ public class N10816 {
 
 	public static void main(String[] args) throws Exception, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+				
 		int N = Integer.parseInt(br.readLine()); // 상근이가 가진 카드의 수 
 	    StringTokenizer st = new StringTokenizer(br.readLine());
 	    
@@ -30,84 +29,67 @@ public class N10816 {
 	    int M = Integer.parseInt(br.readLine()); // 비교대상 카드의 수 
 	    StringTokenizer st2 = new StringTokenizer(br.readLine());
 	    int []tarCard = new int[M]; // 비교대상 카드의 배열 
-	    int []answer = new int[M];
 	    for(int i=0;i<M;i++)
 	    {
 	    	tarCard[i]=Integer.parseInt(st2.nextToken());
 	    }
 	    
+	     StringBuilder sb = new StringBuilder();
 	    for(int i=0;i<M;i++)
 	    {
-	    		int result = BinarySearch(card,tarCard[i]);
-	    		answer[i] = result;
-	    	
-	    	
-	    }
-	    for(int i=0;i<M;i++)
-	    {
-	    	bw.write(answer[i]+" ");;
+	    	sb.append(UpperBound(card,tarCard[i])-LowerBound(card,tarCard[i])).append(' ');		    
 	    }
 	    
-	    bw.flush();
-	    bw.close();
-	    
+	    System.out.print(sb);
+	
 	    
 
 	}
 	
-	public static int BinarySearch(int []arr, int target)
+	public static int UpperBound(int []arr, int target)
 	{
-		int low = 0;
-		int high = arr.length-1;
-		int mid = 0;
+		int lo =0;
+		int up = arr.length;
 		
-		int result = 0;
-		while(low<=high)
+		
+		while(up>lo)
 		{
-			mid = (low+high)/2;
-			int left = mid-1;
-			int right = mid+1;
-			if(arr[mid]==target)
+			int mid = 0;
+			mid = (lo+up)/2;
+			if(target<arr[mid])
 			{
-			     result++;
-			  while(left<arr.length&&right<arr.length&&left>=0)
-			  {
-			 
-				  if(arr[right]==target&&arr[left]==target)
-				  {
-					  result = result+2;
-					  left--;
-					  right++;
-				  } 
-				  else if(arr[left]==target&&arr[right]!=target)
-				  {
-					  left--;
-					  result++;
-				  }
-				  else if(arr[right]==target&&arr[left]!=target)
-				  {
-					  right++;
-					  result++;
-				  }
-				  else if(arr[left]!=target&&arr[right]!=target)
-				  {
-				      break;
-				  }else if(left>=arr.length||right>=arr.length)
-				  {
-				      break;
-				  }
-			  }return result;
-				  
-			 
-			}else if(arr[mid]<target)
+				up = mid;
+			}
+			else
 			{
-				low = mid+1;
-			}else if(arr[mid]>target)
-			{
-				high = mid-1;
+				lo = mid + 1;
 			}
 		}
-		return result;
+		return lo;
+	
 	}
+	public static int LowerBound(int []arr, int target)
+	{
+		int lo =0;
+		int up = arr.length;
+		
+		
+		while(up>lo)
+		{
+			int mid = 0;
+			mid = (lo+up)/2;
+			if(target<=arr[mid])
+			{
+				up = mid;
+			}
+			else
+			{
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	
+	}
+
 
 }
