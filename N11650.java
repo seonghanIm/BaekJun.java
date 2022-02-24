3ea4b5bd4d688900c5bd8ac8d272b8b33e6f9923
SeongHan Im <80394582+seonghanIm@users.noreply.github.com>
2022-02-24 17:00:07 +0900

    Add files via upload

---
+++ N11650.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class N11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] num = new int[N][2];
+
+        for(int i=0;i<N;i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            num[i][0] = Integer.parseInt(st.nextToken());
+            num[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num, (o1, o2) -> {
+            if(o1[0]==o2[0]){
+                return Integer.compare(o1[1],o2[1]);
+            }else{
+                return Integer.compare(o1[0],o2[0]);
+            }
+
+        });
+        for(int i=0;i<N;i++) {
+            System.out.print(num[i][0]+" ");
+            System.out.println(num[i][1]);
+        }
+
+
+    }
+
+}
