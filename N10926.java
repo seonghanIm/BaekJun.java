3f11adbe25044a427cce7cdddc8621a51efe6f03
SeongHan Im <80394582+seonghanIm@users.noreply.github.com>
2022-02-27 20:37:23 +0900

    Add files via upload

---
+++ N10926.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = br.readLine();
+        name +="??!";
+
+        System.out.println(name);
+    }
+
+}
